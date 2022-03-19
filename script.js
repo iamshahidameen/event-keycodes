74720cb724f461f9417fdbef53c22b042fd820a7
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-03-20 01:52:27 +0500

    Added simple user interface using HTML and CSS

---
+++ script.js
@@ -0,0 +1,20 @@
+/*  My method*/
+
+/*
+const main = document.body;
+main.addEventListener('keyup', (e) => {
+    main.innerHTML = `
+    <div>
+        <div>event.KeyPressed</div>
+        <div class="key keyPressed">${e.key}</div>
+    </div>
+    <div>
+        <div>event.KeyCode</div>
+    <div class="key keyCode">${e.keyCode}</div>
+    </div>
+    <div>
+        <div>event.Code</div>
+    <div class="key keyCode">${e.code}</div>
+    </div>
+    `;
+}) */
